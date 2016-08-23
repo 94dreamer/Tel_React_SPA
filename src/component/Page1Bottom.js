9bf247f5c9bd06fce4b0d5e6509d0cc1e2fa0eb4
94dreamer <503633021@qq.com>
2016-08-23 18:25:04 +0800

    update

--- src/component/Page1Bottom.js
+++ src/component/Page1Bottom.js
@@ -2,11 +2,37 @@
  * Created by zz on 2016/8/19.
  */
 import React,{Component} from 'react';
+import FootPage from '../component/FootPage';
 
 class Page1Bottom extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      page: 1
+    }
+    this.turnPage = this.turnPage.bind(this);
+  }
+
+  turnPage(index) {
+    this.setState({
+      page: index
+    });
+  }
+
+  componentDidMount() {
+    /*装载完成阶段调用ajax渲染table明细*/
+    fetch("./table.json").then(function (response) {
+      return response.json();
+    }).then(function (data) {
+      console.log(data);
+      if(data.result.code===0){
+        this.setState(data.result)
+      }else{
+        alert("请求数据失败")
+      }
+    }).catch(function (e) {
+      console.log("Oops, error");
+    });
   }
 
   render() {
@@ -49,52 +75,51 @@ class Page1Bottom extends Component {
             </figure>
             <input id="keyword" type="text" placeholder="请输入公司名、门店名、经纪人姓名、经纪人手机号"/></div>
           <div className="log-table log-table-sales">
-            <table style={{cellpadding:0,cellspacing:0,width:100+'%'}} data-total="3289">
-              <tbody>
-              <tr>
-                <th width="3%"><i className="btn-check"></i></th>
-                <th width="3%">序号</th>
-                <th width="6%">客户</th>
-                <th width="10%">归属</th>
-                <th width="6%">责任销售</th>
-                <th width="6%">拜访时间</th>
-                <th width="6%">通话时长</th>
-                <th width="6%">客户意向</th>
-                <th>沟通详情</th>
-                <th width="6%">跟进结果</th>
-                <th width="6%">呼叫状态</th>
-                <th width="6%">呼叫类型</th>
-                <th width="10%">电销姓名</th>
-              </tr>
-              <tr>
-                <td><i data-uid="8337841" className="btn-check"></i></td>
-                <td>1</td>
-                <td><a target="'_blank'" href="/saletel/record?citycode=bj&amp;uid=8337841&amp;groupid=333">蔡亚运</a></td>
-                <td>麦田房产 合生创展店</td>
-                <td>郭琳琳</td>
-                <td>2016/08/22 12:24:05</td>
-                <td></td>
-                <td>B暂无意向</td>
-                <td title="客户培训" style={{cursor:"help"}}> 客户培训</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-              </tr>
-              </tbody>
-            </table>
-            <p className="callnum">共找到经纪人<span>3289</span>位</p></div>
+            {!this.state.data || !this.state.data.list.length ?
+              <div className="side-null"></div>
+              :
+              <div>
+                <table style={{cellpadding:0,cellspacing:0,width:100+'%'}} data-total="3289">
+                  <tbody>
+                  <tr>
+                    <th width="3%"><i className="btn-check"></i></th>
+                    <th width="3%">序号</th>
+                    <th width="6%">客户</th>
+                    <th width="10%">归属</th>
+                    <th width="6%">责任销售</th>
+                    <th width="6%">拜访时间</th>
+                    <th width="6%">通话时长</th>
+                    <th width="6%">客户意向</th>
+                    <th>沟通详情</th>
+                    <th width="6%">跟进结果</th>
+                    <th width="6%">呼叫状态</th>
+                    <th width="6%">呼叫类型</th>
+                    <th width="10%">电销姓名</th>
+                  </tr>
+                  <tr>
+                    <td><i data-uid="8337841" className="btn-check"></i></td>
+                    <td>1</td>
+                    <td><a target="'_blank'" href="/saletel/record?citycode=bj&amp;uid=8337841&amp;groupid=333">蔡亚运</a>
+                    </td>
+                    <td>麦田房产 合生创展店</td>
+                    <td>郭琳琳</td>
+                    <td>2016/08/22 12:24:05</td>
+                    <td></td>
+                    <td>B暂无意向</td>
+                    <td title="客户培训" style={{cursor:"help"}}> 客户培训</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                  </tbody>
+                </table>
+                <p className="callnum">共找到经纪人<span>{this.state.total}</span>位</p>
+              </div>
+            }
+          </div>
           <div className="main-foot">
-            <ul className="turn-page clearfix">
-              <li className="prev dn"><a href="###">上一页</a></li>
-              <li className="page"><a href="###" className="active">1</a></li>
-              <li className="page"><a href="###">2</a></li>
-              <li className="page"><a href="###">3</a></li>
-              <li className="page"><a href="###">4</a></li>
-              <li className="page"><a href="###">5</a></li>
-              <li className="next "><a href="###">下一页</a></li>
-              <li className="pl5 total">共<span>165</span>页</li>
-            </ul>
+            <FootPage total={110} num={10} active={this.state.page} turnPage={this.turnPage}/>
           </div>
         </div>
       </div>
