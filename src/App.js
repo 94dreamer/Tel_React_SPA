5b8b69aa5d6be480e226eb371337559e3489db15
94dreamer <503633021@qq.com>
2016-08-24 11:00:47 +0800

    12312

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     super(props);
     this.state = {
       level: window.xkTel.level,//1代表销售，0代表经理
-      isCallCenter: window.xkTelInfo.isCallCenter//是否是呼叫中心
+      isCallCenter: window.xkTelInfo.isCallCenter,//是否是呼叫中心
+      jobid:window.xkTel.jobid,
+      login_jobid:window.xkTel.login_jobid
     }
   }
 
@@ -21,8 +23,8 @@ class App extends Component {
           <header id="tel-bar" className="telemarket-hd clearfix" style={{position: "relative"}}>
             <a className="bkl dn"><em className="ico-n"></em>保存并退出</a>
           </header>
-          <Page1Top level={this.state.level} isCallCenter={this.state.isCallCenter} />
-          <Page1Bottom />
+          <Page1Top {...this.state} />
+          <Page1Bottom {...this.state} />
         </section>
       </div>
     );
--- src/component/Page1Bottom.js
+++ src/component/Page1Bottom.js
@@ -21,14 +21,13 @@ class Page1Bottom extends Component {
 
   componentDidMount() {
     /*装载完成阶段调用ajax渲染table明细*/
-    fetch("./table.json").then(function (response) {
+    fetch("./table.json", {credentials: 'include'}).then(function (response) {
       return response.json();
     }).then(function (data) {
-      console.log(data);
-      if(data.result.code===0){
+      if (data.result.code === 0) {
         this.setState(data.result)
-      }else{
-        alert("请求数据失败")
+      } else {
+        alert("data.result.message");
       }
     }).catch(function (e) {
       console.log("Oops, error");
@@ -77,45 +76,79 @@ class Page1Bottom extends Component {
           <div className="log-table log-table-sales">
             {!this.state.data || !this.state.data.list.length ?
               <div className="side-null"></div>
-              :
+              : (!this.props.level?
               <div>
-                <table style={{cellpadding:0,cellspacing:0,width:100+'%'}} data-total="3289">
-                  <tbody>
-                  <tr>
-                    <th width="3%"><i className="btn-check"></i></th>
-                    <th width="3%">序号</th>
-                    <th width="6%">客户</th>
-                    <th width="10%">归属</th>
-                    <th width="6%">责任销售</th>
-                    <th width="6%">拜访时间</th>
-                    <th width="6%">通话时长</th>
-                    <th width="6%">客户意向</th>
-                    <th>沟通详情</th>
-                    <th width="6%">跟进结果</th>
-                    <th width="6%">呼叫状态</th>
-                    <th width="6%">呼叫类型</th>
-                    <th width="10%">电销姓名</th>
-                  </tr>
-                  <tr>
-                    <td><i data-uid="8337841" className="btn-check"></i></td>
-                    <td>1</td>
-                    <td><a target="'_blank'" href="/saletel/record?citycode=bj&amp;uid=8337841&amp;groupid=333">蔡亚运</a>
-                    </td>
-                    <td>麦田房产 合生创展店</td>
-                    <td>郭琳琳</td>
-                    <td>2016/08/22 12:24:05</td>
-                    <td></td>
-                    <td>B暂无意向</td>
-                    <td title="客户培训" style={{cursor:"help"}}> 客户培训</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                  </tr>
-                  </tbody>
-                </table>
-                <p className="callnum">共找到经纪人<span>{this.state.total}</span>位</p>
-              </div>
+              <table cellpadding="0" cellspacing="0" style={{width:100+"%"}} data-total="<%=data.total%>">
+              <tr>
+                (this.props.xkTel.jobid===this.props.xkTel.login_jobid?<th width="3%"><i class="btn-check"></i></th>:null)
+                <th width="3%">序号</th>
+                <th width="6%">客户</th>
+                <th width="10%">归属</th>
+                <th width="8%">责任销售</th>
+                <th width="8%">拜访时间</th>
+                <th width="8%">通话时长</th>
+                <th width="8%">客户意向</th>
+                <th >沟通详情</th>
+                <th width="6%">跟进结果</th>
+                <th width="6%">呼叫状态</th>
+                <th width="6%">呼叫类型</th>
+                <th width="10%">电销姓名</th>
+              </tr>
+              for(let i=0;i< this.state.data.list.length;i++){
+              var list = data.list[i]
+              <tr>
+              <%if(window.xkTel.jobid==window.xkTel.login_jobid){%>
+              <td><%if(list.queueinfo.is_call=="1"){%><i data-uid="<%=list.basicinfo.uid%>" class="btn-check"></i><%}%></td>
+              <%}%>
+              <td><%=list.prekey%></td>
+
+              <%if(window.$_GET['groupid']){%>
+              <td><a target='_blank'
+              href="/saletel/record?citycode=<%=window.xkTel.citycode%>&uid=<%=list.basicinfo.uid%>&groupid=<%=window.$_GET['groupid']%>"><%=list.basicinfo.name%></a>
+              </td><!--客户-->
+              <%}else{%>
+              <td><a target='_blank'
+              href="/saletel/record?citycode=<%=window.xkTel.citycode%>&uid=<%=list.basicinfo.uid%>&jobid=<%=window.$_GET['jobid']%>"><%=list.basicinfo.name%></a>
+              </td><!--客户-->
+              <%}%>
+
+              <td><%=list.basicinfo.companyshortname%> <%=list.basicinfo.storename%></td><!--公司归属-->
+
+              <%if(list.saleinfo.major){%>
+              <td><%=list.saleinfo.major.name%></td><!--责任销售-->
+              <%}else{%>
+              <td><%=list.saleinfo.server_line && list.saleinfo.server_line.name%></td><!--服务销售-->
+              <%}%>
+
+              <td><%if(list.telvisitinfo.visit_time_view){%><%=list.telvisitinfo.visit_time_view%><%}%></td><!--拜访时间-->
+              <td><%=list.telvisitinfo.holdingtime%></td><!--通话时长-->
+              <td><%=list.telvisitinfo.buylevel_view%></td><!--客户意向-->
+              <%var tel_content=[];%>
+              <%if(list.telvisitinfo.visitway==3){%>
+              <%list.telvisitinfo.tomeet_view && tel_content.push('销售是否面见：'+list.telvisitinfo.tomeet_view);
+              list.telvisitinfo.isremembersale_view && tel_content.push('是否记得销售姓名：'+list.telvisitinfo.isremembersale_view);
+              list.telvisitinfo.isknowtopshow_view && tel_content.push('是否知道认证房置顶展示：'+list.telvisitinfo.isknowtopshow_view);
+              list.telvisitinfo.howlongtostore_view && tel_content.push('销售多久去一次门店：'+list.telvisitinfo.howlongtostore_view);
+              list.telvisitinfo.howlongcommunicate_view && tel_content.push('沟通了多久：'+list.telvisitinfo.howlongcommunicate_view);
+              list.telvisitinfo.satisfiedservice_view && tel_content.push('是否满意销售服务：'+list.telvisitinfo.satisfiedservice_view);
+              list.telvisitinfo.remark && tel_content.push('备注说明：'+list.telvisitinfo.remark);%>
+              <%}else{list.telvisitinfo.tag && tel_content.push(list.telvisitinfo.tag);list.telvisitinfo.remark && tel_content.push(list.telvisitinfo.remark.replace(/.{30}/ig,"$&"+"\n"));}%>
+              <td title="<%=tel_content.join('\r')%>" style="cursor: help;">
+              <%var tel_content_join=tel_content.join('');%>
+              <%if(tel_content_join.length>10){%>
+              <%=tel_content_join.substring(0,10)+"..."%>
+              <%}else{%>
+              <%=tel_content_join%>
+              <%}%>
+              </td><!--沟通详情-->
+              <td><%=list.queueinfo.follow_status_view%></td><!--跟进结果-->
+              <td><%=list.queueinfo.call_status_view%></td><!--呼叫状态-->
+              <td><%=list.telvisitinfo.calltype_view%></td><!--呼叫类型-->
+              <td><%if(list.telvisitinfo.visitway==3 && list.telsaleinfo){%><%=list.telsaleinfo.group_name%>-<%=list.telsaleinfo.name%><%}%></td><!--电销姓名-->
+              </tr>
+              <%}%>
+            </table>
+              <p class="callnum">共找到经纪人<span><%=data.total%></span>位</p></div>:null)
             }
           </div>
           <div className="main-foot">
@@ -126,4 +159,5 @@ class Page1Bottom extends Component {
     )
   }
 }
+class TableTr extends
 export default Page1Bottom;
